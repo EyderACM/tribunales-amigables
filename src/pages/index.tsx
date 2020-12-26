860f9e5b91a3029ed36c5b4ab643396c6912021e
eyderacm <eyderacm@gmail.com>
2020-12-26 16:24:28 -0600

    Setup: initialize libraries and dependencies

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import { Flex } from "@chakra-ui/react";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Social Good</title>
+        <link rel="icon" />
+      </Head>
+      <div>
+
+      </div>
+    </div>
+  );
+}
