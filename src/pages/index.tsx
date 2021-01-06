ca9e2a80e1446d41e313a5300553c0c97c2d78f3
Alan Morales <alan_jesus99@hotmail.com>
2021-01-06 10:37:03 -0600

    mid fidelity layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,13 @@
 import Head from "next/head";
+import {
+  Grid,
+  Flex,
+  Stack,
+  Avatar,
+  Heading,
+  Button,
+  Spacer,
+} from "@chakra-ui/react";
 import { MenuCard } from "../../components/molecules/MenuCard";
 
 export default function Home() {
@@ -8,11 +17,30 @@ export default function Home() {
         <title>Social Good</title>
         <link rel="icon" />
       </Head>
-      <div>
-        {/* dummy cards */}
-        <MenuCard label="Prevención" />
-        <MenuCard label="Prevención" />
-      </div>
+      <Grid h="100vh" templateRows="auto 10px 1fr 10px auto" p="20px">
+        <Flex justify="space-between">
+          <Avatar />
+          <Heading>De Boca en Boca</Heading>
+          <Button colorScheme="blue">Entrar</Button>
+        </Flex>
+        <Spacer />
+        <Stack
+          direction="row"
+          spacing="40px"
+          justify="center"
+          align="center"
+          wrap="wrap"
+        >
+          <MenuCard label="Prevención" />
+          <MenuCard label="Cultura Legal" />
+        </Stack>
+        <Spacer />
+        <Grid placeItems="center">
+          <Button colorScheme="green" w="fit-content">
+            Acerca del programa
+          </Button>
+        </Grid>
+      </Grid>
     </div>
   );
 }
