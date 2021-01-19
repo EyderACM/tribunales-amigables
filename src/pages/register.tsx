867e6c7f71e44c997e0afc4cd73561d2ce628d82
Alan Morales <alan_jesus99@hotmail.com>
2021-01-18 19:31:44 -0600

    feat: mid fidelity layout for login and register views

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,12 +1,39 @@
-import { Heading } from "@chakra-ui/react";
-import { Link } from "../components/atoms/Link";
+import {
+  Heading,
+  Grid,
+  Spacer,
+  Stack,
+  Button,
+  Divider,
+  Image,
+} from "@chakra-ui/react";
+import { Input } from "../components/atoms/Input";
 
 const Register = () => {
   return (
-    <>
-      <Heading>Registro</Heading>
-      <Link href="/login">¿Ya tienes una cuenta? inicia sesión :)</Link>
-    </>
+    <Grid h="100vh" templateRows="auto 40px 1fr" p="20px" align="center">
+      <Heading>Regístrate</Heading>
+      <Spacer />
+      <form action="">
+        <Image
+          src="/images/happy-cat.svg"
+          boxSize="100px"
+          objectFit="contain"
+          transform="translateY(9px)"
+        />
+        <Divider opacity={1} borderWidth="5px" borderColor="black" />
+        <Stack spacing="15px" p="10px 40px" maxWidth="500px">
+          <Input placeholder="Nombre" />
+          <Input placeholder="Apellidos" />
+          <Input placeholder="Estado" />
+          <Input placeholder="Correo" />
+          <Input placeholder="Contraseña" />
+          <Button colorScheme="green" type="submit">
+            Registrar
+          </Button>
+        </Stack>
+      </form>
+    </Grid>
   );
 };
 
