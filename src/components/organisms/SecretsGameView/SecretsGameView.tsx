9ccd3c15bcb35debbd13ff177ad6b89ff5264633
Alan Morales <alanmoraales845@gmail.com>
2021-01-23 15:17:47 -0600

    feat: add correct check animation

--- src/components/organisms/SecretsGameView/SecretsGameView.tsx
+++ src/components/organisms/SecretsGameView/SecretsGameView.tsx
@@ -1,7 +1,7 @@
-import { useContext, useEffect } from "react";
-import { Button, Heading } from "@chakra-ui/react";
-import { MenuCard } from "../../molecules/MenuCard";
-import { SecretsGameContext } from "src/components/context/SecretsGameProvider";
+import { useContext } from "react";
+import { Button, Center, Heading, Image, ScaleFade } from "@chakra-ui/react";
+import { MenuCard } from "components/molecules/MenuCard";
+import { SecretsGameContext } from "components/context/SecretsGameProvider";
 
 export const SecretsGameView = () => {
   const {
@@ -9,6 +9,7 @@ export const SecretsGameView = () => {
     changeToNextQuestion,
     changeToPreviusQuestion,
     onAnswerSelected,
+    correctCheckIsShowing,
   } = useContext(SecretsGameContext);
 
   return (
@@ -25,6 +26,18 @@ export const SecretsGameView = () => {
       />
       <Button onClick={changeToPreviusQuestion}>previus</Button>
       <Button onClick={changeToNextQuestion}>next</Button>
+      <Center
+        w="100%"
+        h="100vh"
+        position="absolute"
+        zIndex={5}
+        top="0px"
+        display={correctCheckIsShowing ? "grid" : "none"}
+      >
+        <ScaleFade initialScale={0.9} in={correctCheckIsShowing}>
+          <Image src="/images/correct-answer.svg" boxSize="70%" />
+        </ScaleFade>
+      </Center>
     </>
   );
 };
