fd121286a8919630290d178a249f14cebe587a79
Alan Morales <alanmoraales845@gmail.com>
2021-01-23 15:19:23 -0600

    displaying instructions and results views

--- src/pages/games/secrets.tsx
+++ src/pages/games/secrets.tsx
@@ -1,35 +1,37 @@
-import { SecretsGameProvider } from "src/components/context/SecretsGameProvider";
-import { SecretsGameView } from "src/components/organisms/SecretsGameView";
-import IBinaryQuestion from "src/interfaces/IBinaryQuestion";
-
-const testQuestionsData: IBinaryQuestion[] = [
-  {
-    description: "question 1",
-    correctAnswer: "correct answer",
-    incorrectAnswer: "incorrect answer",
-    isAnswered: false,
-    answerWasCorrect: false,
-  },
-  {
-    description: "question 2",
-    correctAnswer: "correct answer",
-    incorrectAnswer: "incorrect answer",
-    isAnswered: false,
-    answerWasCorrect: false,
-  },
-  {
-    description: "question 3",
-    correctAnswer: "correct answer",
-    incorrectAnswer: "incorrect answer",
-    isAnswered: false,
-    answerWasCorrect: false,
-  },
-];
+import { When } from "react-if";
+import { Heading } from "@chakra-ui/react";
+import { SecretsGameProvider } from "components/context/SecretsGameProvider";
+import { SecretsGameView } from "components/organisms/SecretsGameView";
+import { GameInstructions } from "components/organisms/GameInstructions";
+import { useGameFlow } from "hooks";
+import { testQuestionsData, testInstructionsData } from "testData";
 
 const Secrets = () => {
+  const {
+    changeToGameView,
+    changeToResultsView,
+    inInstructionView,
+    inGameView,
+    inResultsView,
+  } = useGameFlow();
+
   return (
-    <SecretsGameProvider initialQuestionsData={testQuestionsData}>
-      <SecretsGameView />
+    <SecretsGameProvider
+      initialQuestionsData={testQuestionsData}
+      changeToResultsView={changeToResultsView}
+    >
+      <When condition={inInstructionView}>
+        <GameInstructions
+          data={testInstructionsData}
+          changeToGameView={changeToGameView}
+        />
+      </When>
+      <When condition={inGameView}>
+        <SecretsGameView />
+      </When>
+      <When condition={inResultsView}>
+        <Heading>Results</Heading>
+      </When>
     </SecretsGameProvider>
   );
 };
