5517c7660ba9cccb86ae5e5a45533178d4f62408
Alan Morales <alanmoraales845@gmail.com>
2021-01-24 14:13:06 -0600

    test: modify test data

--- src/testData/testQuestionsData.ts
+++ src/testData/testQuestionsData.ts
@@ -2,7 +2,8 @@ import IBinaryQuestion from "interfaces/IBinaryQuestion";
 
 export const testQuestionsData: IBinaryQuestion[] = [
   {
-    description: "question 1",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Augue pulvinar leo morbi quam. Molestie interdum eget in ligula.",
     correctAnswer: "correct answer",
     incorrectAnswer: "incorrect answer",
     isAnswered: false,
