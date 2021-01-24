29ab29ad9503b396a4316b19c2d1705baaa62888
Alan Morales <alanmoraales845@gmail.com>
2021-01-24 14:17:33 -0600

    feat: add SecretsGameResults and a template for game results

--- src/components/organisms/SecretsGameView/SecretsGameView.tsx
+++ src/components/organisms/SecretsGameView/SecretsGameView.tsx
@@ -1,31 +1,61 @@
 import { useContext } from "react";
-import { Button, Center, Heading, Image, ScaleFade } from "@chakra-ui/react";
-import { MenuCard } from "components/molecules/MenuCard";
+import {
+  Button,
+  Center,
+  Image,
+  ScaleFade,
+  Grid,
+  Spacer,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 import { SecretsGameContext } from "components/context/SecretsGameProvider";
+import { GameHeader } from "components/molecules/GameHeader";
+import { ClickableImage } from "components/atoms/ClickableImage";
 
 export const SecretsGameView = () => {
   const {
     currentQuestion,
-    changeToNextQuestion,
-    changeToPreviusQuestion,
     onAnswerSelected,
     correctCheckIsShowing,
+    changeToResultsView,
   } = useContext(SecretsGameContext);
 
   return (
     <>
-      <div>Secrets View</div>
-      <Heading>{currentQuestion.description}</Heading>
-      <MenuCard
-        label="incorrect"
-        onClick={() => onAnswerSelected(currentQuestion.incorrectAnswer)}
-      />
-      <MenuCard
-        label="correct"
-        onClick={() => onAnswerSelected(currentQuestion.correctAnswer)}
-      />
-      <Button onClick={changeToPreviusQuestion}>previus</Button>
-      <Button onClick={changeToNextQuestion}>next</Button>
+      <Grid h="100vh" templateRows="auto 10px 1fr 10px auto" p="20px">
+        <GameHeader gameName="Secretos" />
+        <Spacer />
+        <Stack spacing="40px" align="center">
+          <Text fontSize="xl" textAlign="center" maxWidth="500px">
+            {currentQuestion.description}
+          </Text>
+          <Stack
+            direction="row"
+            spacing="50px"
+            justify="center"
+            align="center"
+            wrap="wrap"
+          >
+            <ClickableImage
+              onClick={() => onAnswerSelected(currentQuestion.incorrectAnswer)}
+            />
+            <ClickableImage
+              onClick={() => onAnswerSelected(currentQuestion.correctAnswer)}
+            />
+          </Stack>
+        </Stack>
+        <Spacer />
+        <Grid placeItems="center">
+          <Button
+            colorScheme="green"
+            w="fit-content"
+            onClick={changeToResultsView}
+          >
+            Finalizar
+          </Button>
+        </Grid>
+      </Grid>
       <Center
         w="100%"
         h="100vh"
