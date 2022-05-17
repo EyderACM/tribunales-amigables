46cf0de4cac321d5500fc1035b1a092ac00936d6
MCAlberto55 <alberto99emc@outlook.com>
2022-05-17 01:30:30 -0500

    Added design

--- src/components/molecules/InstructionCard/InstructionCard.tsx
+++ src/components/molecules/InstructionCard/InstructionCard.tsx
@@ -1,5 +1,14 @@
-import { Center, Heading, Box, Text } from "@chakra-ui/react";
+import {
+  Center,
+  Heading,
+  Box,
+  Text,
+  Flex,
+  Grid,
+  GridItem,
+} from "@chakra-ui/react";
 import colors from "./InstructionCardColors";
+import { Image } from "@chakra-ui/react";
 
 interface IInstructionCard {
   title: string;
@@ -13,18 +22,50 @@ export const InstructionCard = ({ title, information }: IInstructionCard) => {
         width="80%"
         h="500px"
         bg={colors.FILL_COLOR}
-        borderWidth="10px"
         borderColor={colors.BORDER_COLOR}
-        borderRadius="10px"
+        borderRadius="17px"
       >
         <Center>
-          <Heading>{title}</Heading>
+          <Heading fontSize="3em">{title}</Heading>
         </Center>
-        <Center>
-          <Text width="100%" align="center" fontSize="28px">
-            {information}
-          </Text>
-        </Center>
+
+        <Box w="90%" h="70%" margin="auto">
+          <Grid
+            w="100%"
+            h="100%"
+            templateRows="repeat(4, 1fr)"
+            templateColumns="repeat(3, 1fr)"
+            gap={0}
+          >
+            <GridItem rowSpan={3} colSpan={1} bg="">
+              <Image src="/images/TigreIcon.svg" boxSize="100%" />
+            </GridItem>
+            <GridItem rowSpan={2} colSpan={1} bg="">
+              <Flex
+                height="100%"
+                flexDirection="column"
+                justifyContent="flex-end"
+              >
+                <Image src="/images/chatIcon.svg" width="90%" />
+              </Flex>
+            </GridItem>
+            <GridItem rowSpan={1} colSpan={1} bg="" />
+            <GridItem rowSpan={3} colSpan={1} bg="">
+              <Image src="/images/RobotoIcon.svg" boxSize="100%" />
+            </GridItem>
+            <GridItem rowSpan={2} colSpan={1} bg="">
+              <Flex
+                height="100%"
+                flexDirection="column"
+                justifyContent="flex-start"
+              >
+                <Flex height="100%" justifyContent="flex-end">
+                  <Image src="/images/chatIcon.svg" width="90%" />
+                </Flex>
+              </Flex>
+            </GridItem>
+          </Grid>
+        </Box>
       </Box>
     </Center>
   );
