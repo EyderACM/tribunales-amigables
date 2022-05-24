4bcc4a141b50b9302d2f2eb0b262c79a5a40a33b
MCAlberto55 <alberto99emc@outlook.com>
2022-05-24 18:44:52 -0500

    added responsive character dialog

--- src/components/organisms/SecretsGameView/SecretsGameView.tsx
+++ src/components/organisms/SecretsGameView/SecretsGameView.tsx
@@ -9,6 +9,7 @@ import {
   Stack,
   Text,
   Box,
+  Flex,
 } from "@chakra-ui/react";
 import { SecretsGameContext } from "components/context/SecretsGameProvider";
 import { GameHeader } from "components/molecules/GameHeader";
@@ -24,82 +25,99 @@ export const SecretsGameView = () => {
   } = useContext(SecretsGameContext);
 
   return (
-    <Box w="100%" h="100%">
+    <Box w="100%" h="75vh" display="inline-block">
       <GameHeader gameName="Secretos" />
-      <Spacer />
 
-      <Stack spacing="40px" align="center">
-        <Text fontSize="xl" textAlign="center" maxWidth="500px">
-          {currentQuestion.question}
-        </Text>
-        <Image
-          display="inline-block"
-          src="/images/IngameSecretsRoboto.svg"
-          boxSize="10%"
-        />
+      <Box
+        margin="auto"
+        width="80%"
+        height="100%"
+        background="rgba(101, 168, 187, 0.67)"
+      >
+        <Flex width="100%" height="100%">
+          <Box width="50%" margin="auto">
+            <Image
+              width="60%"
+              margin="auto"
+              src="/images/IngameSecretsRoboto.svg"
+            />
+          </Box>
 
-        <Button
-          width="30%"
-          margin="auto"
-          bg={"#7DB778"}
-          fontWeight="Regular"
-          border="0.5px solid "
-          borderColor={"#7DB778"}
-          borderRadius="6px"
-          boxShadow="0px 5px 0px #62915E"
-          display="block"
-          alt="contar secreto"
-          onClick={() => onAnswerSelected("secreto malo")}
-        >
-          ¡Contarlo!
-        </Button>
-        <Button
-          width="30%"
-          margin="auto"
-          bg={"#7DB778"}
-          fontWeight="Regular"
-          border="0.5px solid "
-          borderColor={"#7DB778"}
-          borderRadius="6px"
-          boxShadow="0px 5px 0px #62915E"
-          display="block"
-          alt="guardar secreto"
-          onClick={() => onAnswerSelected("secreto bueno")}
-        >
-          No contarlo
-        </Button>
-        <Button
-          width="30%"
-          margin="auto"
-          bg={"#7DB778"}
-          fontWeight="Regular"
-          border="0.5px solid "
-          borderColor={"#7DB778"}
-          borderRadius="6px"
-          boxShadow="0px 5px 0px #62915E"
-          display="block"
-        >
-          No lo se
-        </Button>
+          <Box
+            width="45%"
+            height="80%"
+            margin="auto"
+            backgroundImage='url("/images/LongChat.svg")'
+            backgroundRepeat="no-repeat"
+            bgSize="100% 100%"
+          >
+            <Flex
+              width="100%"
+              height="70%"
+              flexDirection="column"
+              justifyContent="space-around"
+              alignItems="center"
+            >
+              <Text fontSize="xl" textAlign="center" maxWidth="500px">
+                {currentQuestion.question}
+              </Text>
+              <Text fontSize="xl" textAlign="center" maxWidth="500px">
+                ¿Puedo contar el secreto o debería no hacerlo?
+              </Text>
+              <Button /*create componenet*/
+                width="40%"
+                marginTop="5%"
+                bg={"#7DB778"}
+                fontWeight="Regular"
+                border="0.5px solid "
+                borderColor={"#7DB778"}
+                borderRadius="6px"
+                boxShadow="0px 5px 0px #62915E"
+                alt="contar secreto"
+                onClick={() => onAnswerSelected("secreto malo")}
+              >
+                ¡Contarlo!
+              </Button>
+              <Button
+                width="40%"
+                marginTop="5%"
+                bg={"#7DB778"}
+                fontWeight="Regular"
+                border="0.5px solid "
+                borderColor={"#7DB778"}
+                borderRadius="6px"
+                boxShadow="0px 5px 0px #62915E"
+                alt="guardar secreto"
+                onClick={() => onAnswerSelected("secreto bueno")}
+              >
+                No contarlo
+              </Button>
+              <Button
+                width="40%"
+                marginTop="5%"
+                bg={"#7DB778"}
+                fontWeight="Regular"
+                border="0.5px solid "
+                borderColor={"#7DB778"}
+                borderRadius="6px"
+                boxShadow="0px 5px 0px #62915E"
+              >
+                No lo se
+              </Button>
+            </Flex>
+          </Box>
+        </Flex>
 
-        <Stack
-          direction="row"
-          spacing="50px"
-          justify="center"
-          align="center"
-          wrap="wrap"
-        ></Stack>
-      </Stack>
-      <Spacer />
-      <Grid placeItems="center">
-        <Button
-          colorScheme="green"
-          w="fit-content"
-          onClick={changeToResultsView}
-        >
-          Finalizar
-        </Button>
-      </Grid>
+        <Grid placeItems="center">
+          <Button
+            colorScheme="green"
+            w="fit-content"
+            onClick={changeToResultsView}
+          >
+            Finalizar
+          </Button>
+        </Grid>
+      </Box>
       <FadeInOutImage
         src="/images/correct-answer.svg"
         show={correctCheckIsShowing}
